245788474a456201b08befb5ed3b16412ca18144
Brainiac2025 <afeez.pierscon@gmail.com>
2023-06-12 18:01:11 +0100

    field declaration

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
+import javax.swing.*;
+
+public class Main  extends JFrame {
+
+    private JTextField myTextfield;
+    private JTable myTable;
+    private JButton pauseButton, cancelButton, resumeButton, clearButton;
+
+    private DownloadsTableModel myTableModel;
+    private Download selectDownload;
+    private boolean clearing;
+
+    public Main(){
+        setTitle("Download Manager");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
